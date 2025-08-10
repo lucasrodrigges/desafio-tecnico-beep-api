ea86297cd970a9a46131abe19787255b316f7569
lucas <rodriggeslucas@gmail.com>
2025-08-10 19:48:02 -0300

    Update Redis configuration to use UPSTASH_REDIS_URL and remove password handling

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,17 +1,17 @@
 if Rails.env.production?
-  redis_url = ENV['UPSTASH_REDIS_REST_URL']
-  redis_token = ENV['UPSTASH_REDIS_REST_TOKEN']
+  redis_url = ENV['UPSTASH_REDIS_URL']
   
-  if redis_url && redis_token
-    redis_config = {
-      url: redis_url,
-      password: redis_token
-    }
-    
+  if redis_url.blank? && ENV['UPSTASH_REDIS_REST_URL'] && ENV['UPSTASH_REDIS_REST_TOKEN']
+    rest_url = ENV['UPSTASH_REDIS_REST_URL']
+    token = ENV['UPSTASH_REDIS_REST_TOKEN']
+    uri = URI.parse(rest_url)
+    redis_url = "redis://default:#{token}@#{uri.host}:#{uri.port || 6379}"
+  end
+  
+  if redis_url
     ActionCable.server.config.cable = {
       adapter: 'redis',
       url: redis_url,
-      password: redis_token,
       channel_prefix: 'desafio_tecnico_beep_production'
     }
   end
