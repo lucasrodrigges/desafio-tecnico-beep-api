cf1f5dbabf467714cde04e990d890a9b3450c371
lucas <rodriggeslucas@gmail.com>
2025-08-12 09:52:17 -0300

    Refactor make_request method in HttpService to simplify error handling and remove retry logic

--- app/services/http_service.rb
+++ app/services/http_service.rb
@@ -2,32 +2,16 @@ require 'net/http'
 require 'openssl'
 
 class HttpService
-  MAX_RETRIES = 2 
-  RETRY_DELAY = 0.3
-
-  def self.make_request(url, retries = MAX_RETRIES)
+  def self.make_request(url)
+    Rails.logger.debug "[HttpService] Making request to: #{url}"
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    http.open_timeout = 5
-    http.read_timeout = 15
-    http.ssl_timeout = 5
-
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(url)
     response = http.request(request)
-    
-    if response.code == '200'
-      response.body
-    else
-      raise "HTTP Error: #{response.code}"
-    end
+    response.body
   rescue => e
-    if retries > 0
-      Rails.logger.warn "[HttpService] Request failed, retrying... (#{retries} attempts left): #{e.message}"
-      sleep(RETRY_DELAY)
-      make_request(url, retries - 1)
-    else
-      raise e
-    end
+    Rails.logger.error "[HttpService] Request failed: #{e.class} - #{e.message}"
+    raise e
   end
 end
