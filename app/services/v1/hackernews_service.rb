d77d39f43c084fb1802151a3de7aa386e5b3e193
lucas <rodriggeslucas@gmail.com>
2025-08-11 23:04:19 -0300

    Refactor HackernewsService to use batch processing for fetching stories and comments, improving concurrency and performance

--- app/services/v1/hackernews_service.rb
+++ app/services/v1/hackernews_service.rb
@@ -3,21 +3,12 @@ require 'json'
 module V1
   class HackernewsService
     HACKERNEWS_API_BASE_URL = "https://hacker-news.firebaseio.com/v0"
+    MAX_CONCURRENT_THREADS = 10
+    BATCH_SIZE = 50
 
     def fetch_top_stories(limit = 15)
       ids = fetch_top_story_ids.first(limit)
-      stories = []
-      threads = ids.map do |id|
-        Thread.new do
-          story = fetch_story(id)
-          if story
-            relevant_comments = relevant_comments_for_story(id)
-            story['comments'] = relevant_comments || []
-            stories << story
-          end
-        end
-      end
-      threads.each(&:join)
+      stories = process_stories_in_batches(ids)
       stories
     end
 
@@ -26,18 +17,7 @@ module V1
 
       unless stories
         ids = fetch_top_story_ids
-        stories = []
-        threads = ids.map do |id|
-          Thread.new do
-            story = fetch_story(id)
-            if story
-              relevant_comments = relevant_comments_for_story(id)
-              story['comments'] = relevant_comments || []
-              stories << story
-            end
-          end
-        end
-        threads.each(&:join)
+        stories = process_stories_in_batches(ids)
         RedisService.create_top_stories_cache(stories)
       end
 
@@ -54,16 +34,16 @@ module V1
       []
     end
 
+    def fetch_multiple_stories(ids)
+      process_bulk_items_in_batches(ids) { |id| fetch_story(id) }
+    end
+
+    def fetch_multiple_comments(ids)
+      process_bulk_items_in_batches(ids) { |id| fetch_comment(id) }
+    end
+
     def replies_at_comments(ids)
-      comments = []
-      threads = ids.map do |id|
-        Thread.new do
-          comment = fetch_comment(id)
-          comments << comment if comment && comment['text'] && comment['by']
-        end
-      end
-      threads.each(&:join)
-      comments
+      process_comments_in_batches(ids)
     end
 
     def fetch_latest_story_ids
@@ -99,16 +79,10 @@ module V1
     def relevant_comments_for_story(story_id, min_words: 20)
       story = fetch_story(story_id)
       return [] unless story && story['kids']
+      
       comment_ids = story['kids']
-      comments = []
-      threads = comment_ids.map do |comment_id|
-        Thread.new do
-          comment = fetch_comment(comment_id)
-          comments << comment if comment
-        end
-      end
-      threads.each(&:join)
-      filtered = comments.select { |c| c['text'] && c['text'].split.size > min_words }
+      comments = process_comments_in_batches(comment_ids, filter_by_text: false)
+      filtered = filter_comments_in_batches(comments, min_words: min_words)
       filtered.sort_by { |c| -(c['score'] || 0) }
     end
 
@@ -120,23 +94,112 @@ module V1
 
       unless stories
         latest_ids = fetch_latest_story_ids.first(max_ids)
-        stories = []
-        threads = latest_ids.map do |id|
-          Thread.new do
-            story = fetch_story(id)
-            stories << story if story
-          end
-        end
-        threads.each(&:join)
+        stories = process_stories_in_batches(latest_ids, include_comments: false)
         RedisService.create_stories_cache(stories)
       end
 
-      filtered = stories.select do |story|
-        story['title']&.downcase&.include?(keyword) ||
-        story['text']&.downcase&.include?(keyword) ||
-        story['by']&.downcase&.include?(keyword)
-      end
+      filtered = filter_stories_by_keyword_in_batches(stories, keyword)
       filtered.sort_by { |story| -story['time'].to_i }.first(limit)
+    end
+
+    private
+
+    def process_stories_in_batches(ids, include_comments: true)
+      stories = []
+      stories_mutex = Mutex.new
+      
+      ids.each_slice(MAX_CONCURRENT_THREADS) do |batch|
+        threads = batch.map do |id|
+          Thread.new do
+            story = fetch_story(id)
+            if story
+              if include_comments
+                relevant_comments = relevant_comments_for_story(id)
+                story['comments'] = relevant_comments || []
+              end
+              stories_mutex.synchronize { stories << story }
+            end
+          rescue => e
+            Rails.logger.error "[HackernewsService] Error processing story #{id}: #{e.message}"
+          end
+        end
+        threads.each(&:join)
+      end
+      
+      stories
+    end
+
+    def process_comments_in_batches(ids, filter_by_text: true)
+      comments = []
+      comments_mutex = Mutex.new
+      
+      ids.each_slice(MAX_CONCURRENT_THREADS) do |batch|
+        threads = batch.map do |id|
+          Thread.new do
+            comment = fetch_comment(id)
+            if comment
+              should_include = if filter_by_text
+                                comment['text'] && comment['by']
+                              else
+                                true
+                              end
+              comments_mutex.synchronize { comments << comment } if should_include
+            end
+          rescue => e
+            Rails.logger.error "[HackernewsService] Error processing comment #{id}: #{e.message}"
+          end
+        end
+        threads.each(&:join)
+      end
+      
+      comments
+    end
+
+    def filter_comments_in_batches(comments, min_words: 20)
+      filtered_comments = []
+      
+      comments.each_slice(BATCH_SIZE) do |batch|
+        batch_filtered = batch.select do |comment|
+          comment['text'] && comment['text'].split.size > min_words
+        end
+        filtered_comments.concat(batch_filtered)
+      end
+      
+      filtered_comments
+    end
+
+    def filter_stories_by_keyword_in_batches(stories, keyword)
+      filtered_stories = []
+      
+      stories.each_slice(BATCH_SIZE) do |batch|
+        batch_filtered = batch.select do |story|
+          story['title']&.downcase&.include?(keyword) ||
+          story['text']&.downcase&.include?(keyword) ||
+          story['by']&.downcase&.include?(keyword)
+        end
+        filtered_stories.concat(batch_filtered)
+      end
+      
+      filtered_stories
+    end
+
+    def process_bulk_items_in_batches(ids, &block)
+      results = []
+      results_mutex = Mutex.new
+      
+      ids.each_slice(MAX_CONCURRENT_THREADS) do |batch|
+        threads = batch.map do |id|
+          Thread.new do
+            result = block.call(id)
+            results_mutex.synchronize { results << result } if result
+          rescue => e
+            Rails.logger.error "[HackernewsService] Error processing bulk item #{id}: #{e.message}"
+          end
+        end
+        threads.each(&:join)
+      end
+      
+      results
     end
   end
 end
